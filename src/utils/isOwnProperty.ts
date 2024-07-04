1c811ce972ce91a2823e07553c35bf0d549c60a8
tjd985 <tjd985@gmail.com>
2024-07-04 22:45:17 +0900

    ✨ [Feat] 프로퍼티 체크 유틸함수 구현
    hasOwnPropery를 활용한 유틸함수 구현

---
+++ src/utils/isOwnProperty.ts
@@ -0,0 +1,7 @@
+export default function isOwnProperty(object: object, targetProperty: string) {
+  if (Object.prototype.hasOwnProperty.call(object, targetProperty)) {
+    return true;
+  }
+
+  return false;
+}
