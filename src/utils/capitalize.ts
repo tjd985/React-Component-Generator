5100729407470fefcd74d5db38cf869b691d85ae
tjd985 <tjd985@gmail.com>
2024-07-05 16:07:02 +0900

    ✨ [Feat] 유틸함수 추가
    capitalize, getDefaultValue

---
+++ src/utils/capitalize.ts
@@ -0,0 +1,5 @@
+function capitalize(targetString: string) {
+  return targetString[0].toUpperCase() + targetString.slice(1);
+}
+
+export default capitalize;
---
+++ src/utils/getDefaultValue.ts
@@ -0,0 +1,23 @@
+function getDefaultValue(type: string) {
+  switch (type) {
+    case "object":
+      return {};
+
+    case "boolean":
+      return false;
+
+    case "array":
+      return [];
+
+    case "number":
+      return 0;
+
+    case "string":
+      return "";
+
+    default:
+      return null;
+  }
+}
+
+export default getDefaultValue;
